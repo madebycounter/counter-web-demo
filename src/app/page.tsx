ae1ae780a72f0501f1335e0e017eb8d66ba1d20a
William Gardner <william@madebycounter.com>
2024-07-15 14:46:37 -0700

    Add shopify code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
+import { getProducts } from "@/lib/shopify";
+
+export default async function Home() {
+    const data = await getProducts({});
+
+    console.log(data[0].images);
+
     return <main>hey</main>;
 }
